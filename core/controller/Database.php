9034e1122575be6f8095ff7db8717a4cc9c1a46a
LuisValles92 <58343146+LuisValles92@users.noreply.github.com>
2020-10-21 22:08:41 +0200

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bookmedik";
+		$this->user="b0b6c71c06efc4";$this->pass="9e587e4d";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_6d3f2c1dc835e21";
 	}
 
 	function connect(){
